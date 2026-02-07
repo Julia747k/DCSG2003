3d5c394b8bcf1f2bf1289eb4785f61e4a8ba9a24
Ubuntu <ubuntu@manager.openstack.local>
2026-02-07 12:24:03 +0000

    Added new scripts for Oblig 2

---
+++ scripts/run_on_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COMMAND=$1
+
+for server in $(openstack server list | grep Ubu | awk '{ print $8 $9 }' | sed -e 's/.*,//g' | sed -e 's/,.*//g' | sed -e 's///'); do
+      # Print out the IP of the server
+       echo "server: $server";
+
+      # run the command in question
+        ssh root@$server $COMMAND ;
+done
+
