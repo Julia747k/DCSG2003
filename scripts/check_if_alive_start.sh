0277440292122b597fc49f4dfb2b4fe744e75168
Ubuntu <ubuntu@manager.openstack.local>
2026-02-15 12:06:19 +0000

    updates scripts

--- scripts/check_if_alive_start.sh
+++ scripts/check_if_alive_start.sh
@@ -3,25 +3,32 @@
 source /home/ubuntu/DCSG2003_V26_group37-openrc.sh
 
 for server in $(openstack server list | grep Ubu | grep -v manager | awk '{ print $8 $9 }' | sed -e 's/.*,//g' | sed -e 's/.*=//g' | sed -e 's/|//'); do
-	if ping -c 1 -W 1 "$server" > /dev/null 2>&1 ; then 
-		echo "$(date) $server is OK"
+	if ping -c 1 -W 1 "$server" > /dev/null 2>&1 ; then	
+		true;
+#		echo "$(date) $server is OK";
 	else 
 		echo "$(date) $server is DOWN";
 	
 		server_name=$(openstack server list | grep "$server" | awk '{print $4}')
 
 		if [ -n "$server_name" ]; then 
-			echo "$(date) Starting: $server_name ..." 
-			openstack server start "$server_name"
+			echo "$(date) Starting: $server_name ..."; 
+			openstack server start "$server_name";
 			
+			until ping -c 1 -W 1 "$server" > /dev/null 2>&1; do
+				sleep 5;
+			done
+
 			if [ "$server_name" = "db1" ]; then
-				echo "$(date) Restarting yugaDB on $server_name ..."	
-				sleep 60
+				sleep 30;
+#				echo "$(date) Restarting yugaDB on $server_name ...";	
 				ssh ubuntu@$server \
-					"cd /home/ubuntu/yugabyte-2025.2.0.0 && sudo ./bin/yugabyted start --advertise_address=192.168.132.48 --base_dir=/gfdata"
+					"cd /home/ubuntu/yugabyte-2025.2.0.0 && sudo ./bin/yugabyted start --advertise_address=192.168.132.48 --base_dir=/gfdata > /dev/null 2>&1";
+				echo "$(date) YugaDB is back up";
 			fi	
-		else 
-			echo "$(date) ERROR: Could not find server name for $server :("
+		else
+		       true;	
+#			echo "$(date) ERROR: Could not find server name for $server :(";
 		fi
 
 	fi
