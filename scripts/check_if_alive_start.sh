26440ed72c0ddbdda33023c2e7d1240120682d5c
Ubuntu <ubuntu@manager.openstack.local>
2026-02-19 09:52:29 +0000

    Added Yugabyte health check independent of db1 status

--- scripts/check_if_alive_start.sh
+++ scripts/check_if_alive_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /home/ubuntu/DCSG2003_V26_group37-openrc.sh
-
+startDB="cd /home/ubuntu/yugabyte-2025.2.0.0 && sudo ./bin/yugabyted start --advertise_address=192.168.132.48 --base_dir=/gfdata > /dev/null 2>&1"
 for server in $(openstack server list | grep Ubu | grep -v manager | awk '{ print $8 $9 }' | sed -e 's/.*,//g' | sed -e 's/.*=//g' | sed -e 's/|//'); do
 	if ping -c 1 -W 1 "$server" > /dev/null 2>&1 ; then	
 		true;
@@ -22,9 +22,8 @@ for server in $(openstack server list | grep Ubu | grep -v manager | awk '{ prin
 			if [ "$server_name" = "db1" ]; then
 				sleep 30;
 #				echo "$(date) Restarting yugaDB on $server_name ...";	
-				ssh ubuntu@$server \
-					"cd /home/ubuntu/yugabyte-2025.2.0.0 && sudo ./bin/yugabyted start --advertise_address=192.168.132.48 --base_dir=/gfdata > /dev/null 2>&1";
-				echo "$(date) YugaDB is back up";
+				ssh ubuntu@192.168.132.48 "$startDB"
+				echo "$(date) Yugabyte start attempted at DB1";
 			fi	
 		else
 		       true;	
@@ -32,6 +31,15 @@ for server in $(openstack server list | grep Ubu | grep -v manager | awk '{ prin
 		fi
 
 	fi
-
-
 done
+
+if ssh -o BatchMode=yes -o ConnectTimeout=5 ubuntu@192.168.132.48 \
+                "PGCONNECT_TIMEOUT=3 ysqlsh -h 192.168.132.48 -p 5433 -U choso -d bf -c 'SELECT 1;' >/dev/null 2>&1"
+        then
+                true
+#               echo "$(date)YSQL is up"
+        else
+                ssh ubuntu@192.168.132.48 "$startDB"
+                echo "$(date)DB should be up now!"
+
+fi
